01c6b1a8aecc4d2c300a6bf02b237b94e94d203c
charles delannoy <charles.delannoy@hei.fr>
2021-05-15 14:51:46 +0200

    create chatroom method

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,20 @@
 class ChatroomsController < ApplicationController
   def index
+    @chatroom = Chatroom.new
+  end
 
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    if @chatroom.save
+      redirect_to chatrooms_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name)
   end
 end
