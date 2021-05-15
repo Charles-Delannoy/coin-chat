c90ec1fac61477cb9cd76a7d4eacc66fac17168f
charles delannoy <charles.delannoy@hei.fr>
2021-05-15 15:08:56 +0200

    display chatrooms with react

--- app/javascript/components/ChatroomsIndex.js
+++ app/javascript/components/ChatroomsIndex.js
@@ -1,10 +1,30 @@
-import React, { Fragment } from "react"
+import React, { useState, useEffect, Fragment } from "react"
+import ChatroomIndexChannel from 'channels/chatroom_index_channel'
 // import PropTypes from "prop-types"
 
 const ChatroomsIndex = () => {
+
+  const [chatrooms, setChatrooms] = useState([]);
+
+  useEffect(() => {
+    ChatroomIndexChannel.received = (data) => {
+      console.log(data.chatrooms);
+      const rooms = data.chatrooms.map((chatroom) => {
+        return (
+          <a key={chatroom.id} href={`/chatrooms/${chatroom.id}`}>
+            <div>
+              <h2>{chatroom.name}</h2>
+            </div>
+          </a>
+        );
+      });
+      setChatrooms(rooms);
+    }
+  }, []);
+
   return (
     <Fragment>
-      <p>Chatrooms list</p>
+      {chatrooms}
     </Fragment>
   );
 }
