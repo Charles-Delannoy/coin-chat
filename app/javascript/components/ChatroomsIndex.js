3fc23033629ec20602e7826b7279d22869f7c2c9
charles delannoy <charles.delannoy@hei.fr>
2021-05-15 15:00:54 +0200

    install react and display a component in chatrooms index

---
+++ app/javascript/components/ChatroomsIndex.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react"
+// import PropTypes from "prop-types"
+
+const ChatroomsIndex = () => {
+  return (
+    <Fragment>
+      <p>Chatrooms list</p>
+    </Fragment>
+  );
+}
+
+export default ChatroomsIndex
