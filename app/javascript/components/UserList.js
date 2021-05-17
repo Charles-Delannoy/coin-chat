db9329a9b17a1b086b23e3385f85052659f2b7d9
charles delannoy <charles.delannoy@hei.fr>
2021-05-17 10:19:40 +0200

    init UserList component

---
+++ app/javascript/components/UserList.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react"
+import PropTypes from "prop-types"
+const UserList = () => {
+  return (
+    <Fragment>
+      <div className="user-list">
+        <h2>Connected Users</h2>
+      </div>
+    </Fragment>
+  );
+}
+
+export default UserList
